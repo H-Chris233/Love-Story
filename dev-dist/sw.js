aee091e7ce7f3fc2dcc81d2bdcfdd101724132fc
H-Chris233 <h-chris233@outlook.com>
2025-09-26 02:45:48 +0000

    chore: update configuration and clean up duplicate files
    - Update vercel.json to use correct cron endpoint
    - Remove duplicate anniversary-reminders.ts cron function
    - Update service worker file
    - Clean up redundant API function files

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5a3joop0tlg"
+    "revision": "0.9948hn1geig"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
