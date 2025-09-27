1ae18a1bbda0695f069c674ac621f6506bad11b1
H-Chris233 <h-chris233@outlook.com>
2025-09-27 11:00:11 +0000

    chore(pwa): update service worker and workbox files
    - Update service worker (sw.js) with latest PWA configuration
    - Update workbox library file (workbox-86c9b217.js) for caching strategies
    - These are auto-generated files for PWA functionality

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fapqs2mhjs8"
+    "revision": "0.0lgc25cp0qg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
